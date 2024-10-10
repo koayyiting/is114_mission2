d4567f58502a75ce9b905e61d17293c15373b702
koayyiting <koayyiting@gmail.com>
2024-10-10 20:04:49 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+includeHTML();
+
+function includeHTML() {
+  var z, i, a, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    if (z[i].getAttribute("script-html")) {
+      a = z[i].cloneNode(false);
+      file = z[i].getAttribute("script-html");
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          a.removeAttribute("script-html");
+          a.innerHTML = xhttp.responseText;
+          z[i].parentNode.replaceChild(a, z[i]);
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+}
+
+
+
